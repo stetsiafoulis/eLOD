0e40e41b16786055d355ab732dfe16fac5be2cae
stergios <stetsiafoulis@gmail.com>
2016-03-04 23:40:13 +0000

    fill in the directory

--- docker-drupal/files/webfact_status.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /var/www/html/sites/default
-drush status|grep 'Drupal version'|awk '{print $1 $4}'
--- docker-drupal/gitwrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Pass ssh keys to git clone
-# e.g.
-# GIT_SSH=/gitwrap.sh git clone -b master -q git@bitbucket.org:/MYUSER/MYREPO.git html
-
-ssh -o StrictHostKeyChecking=no -i /root/gitwrap/id_rsa -q $@
-
